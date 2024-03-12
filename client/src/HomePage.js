aee8f9f6e8fe80805c2b85353038b8ba30a2c59b
ABERsara <sara0556799675@gmail.com>
2024-03-12 11:11:00 +0200

    modified HomePage added

---
+++ client/src/HomePage.js
@@ -0,0 +1,7 @@
+const HomePage=()=>{
+return <div className="homePage">
+    <h1>Wellcome- we are pleased to host you!</h1>
+    <h2>On this application you will be able to organize your data, tasks, posts and more. <br/>You can start now!<br/></h2>
+</div>
+}
+export default HomePage
